04e717ba27ee1ca98095e2a59ea11b53142e458b
Christine Caulfield <ccaulfie@redhat.com>
2009-01-07 13:57:41 +0000

    config: ldap convertor should use CS_ codes rather than SA_AIS ones
    Signed-off-by: Christine Caulfield <ccaulfie@redhat.com>

---
+++ cman/config/cman.h
@@ -0,0 +1,14 @@
+
+/* How we announce ourself in syslog */
+#define CMAN_NAME "CMAN"
+
+#define MAX_CLUSTER_MEMBER_NAME_LEN 255
+
+/* Defaults for configuration variables */
+#define NOCCS_KEY_FILENAME      "/etc/cluster/cman_authkey"
+#define DEFAULT_PORT            5405
+#define DEFAULT_CLUSTER_NAME    "RHCluster"
+#define DEFAULT_MAX_QUEUED       128
+#define DEFAULT_QUORUMDEV_POLL   10000
+#define DEFAULT_SHUTDOWN_TIMEOUT 5000
+#define DEFAULT_CCSD_POLL        1000
