39140df41477f924bf53b2830892b5ae0dacf0bd
Fabio M. Di Nitto <fdinitto@redhat.com>
2009-01-30 11:03:11 +0100

    build: export INCDIR and LIBDIR to pc files
    Signed-off-by: Fabio M. Di Nitto <fdinitto@redhat.com>

--- make/libs.mk
+++ make/libs.mk
@@ -41,6 +41,8 @@ $(PKGCONF): $(S)/$(PKGCONF).in
 	cat $(S)/$(PKGCONF).in | \
 	sed \
 		-e 's#@PREFIX@#${prefix}#g' \
+		-e 's#@LIBDIR@#${libdir}#g' \
+		-e 's#@INCDIR@#${incdir}#g' \
 		-e 's#@VERSION@#${RELEASE_VERSION}#g' \
 	> $@
 
