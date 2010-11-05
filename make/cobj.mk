9fe9d3725e864b51af24b702992a9a8d92385a3a
Fabio M. Di Nitto <fdinitto@redhat.com>
2010-11-05 05:14:19 -0400

    dlm_controld.pcmk: drop last remaining bits
    Signed-off-by: Fabio M. Di Nitto <fdinitto@redhat.com>

--- make/cobj.mk
+++ make/cobj.mk
@@ -5,10 +5,6 @@
 %_lt.o: $(S)/%.c
 	$(CC) $(CFLAGS) $(EXTRA_CFLAGS) -c -o $@ $<
 
-# used by group/dlm_controld
-%-pcmk.o: $(S)/%.c
-	$(CC) $(CFLAGS) $(EXTRA_CFLAGS) $(PCMK_CFLAGS) -c -o $@ $<
-
 # used by rgmanager/src/daemons
 %-noccs.o: $(S)/%.c
 	$(CC) $(CFLAGS) $(EXTRA_CFLAGS) $(NOCCS_CFLAGS) -c -o $@ $<
