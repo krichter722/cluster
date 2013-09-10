6e2dd4f0129c8b1635bf401668dcb10dca29a9a6
Fabio M. Di Nitto <fdinitto@redhat.com>
2013-09-10 20:41:16 +0200

    build: workaround gcc __FILE__ macro expansion to save fdata size at runtime
    Signed-off-by: Fabio M. Di Nitto <fdinitto@redhat.com>
    Acked-by: Jan Friesse <jfriesse@redhat.com>

--- make/cobj.mk
+++ make/cobj.mk
@@ -1,10 +1,38 @@
+ifdef outoftreebuild
+
 %.o: $(S)/%.c
-	$(CC) $(CFLAGS) $(EXTRA_CFLAGS) $(REENT_CFLAGS) -c -o $@ $<
+	cd $(S) > /dev/null 2>&1 && \
+	src=$(shell basename $<) && \
+	$(CC) $(CFLAGS) $(EXTRA_CFLAGS) $(REENT_CFLAGS) -c -o $(O)/$@ $$src && \
+	cd - >/dev/null 2>&1;
 
 # used by dlm/libdlm
 %_lt.o: $(S)/%.c
-	$(CC) $(CFLAGS) $(EXTRA_CFLAGS) -c -o $@ $<
+	cd $(S) > /dev/null 2>&1 && \
+	src=$(shell basename $<) && \
+	$(CC) $(CFLAGS) $(EXTRA_CFLAGS) -c -o $(O)/$@ $$src && \
+	cd - >/dev/null 2>&1;
 
 # used by rgmanager/src/daemons
 %-noccs.o: $(S)/%.c
-	$(CC) $(CFLAGS) $(EXTRA_CFLAGS) $(NOCCS_CFLAGS) -c -o $@ $<
+	cd $(S) > /dev/null 2>&1 && \
+	src=$(shell basename $<) && \
+	$(CC) $(CFLAGS) $(EXTRA_CFLAGS) $(NOCCS_CFLAGS) -c -o $(O)/$@ $$src && \
+	cd - >/dev/null 2>&1;
+
+else
+%.o: $(S)/%.c
+	src=$(shell basename $<) && \
+	$(CC) $(CFLAGS) $(EXTRA_CFLAGS) $(REENT_CFLAGS) -c -o $@ $$src
+
+# used by dlm/libdlm
+%_lt.o: $(S)/%.c
+	src=$(shell basename $<) && \
+	$(CC) $(CFLAGS) $(EXTRA_CFLAGS) -c -o $@ $$src
+
+# used by rgmanager/src/daemons
+%-noccs.o: $(S)/%.c
+	src=$(shell basename $<) && \
+	$(CC) $(CFLAGS) $(EXTRA_CFLAGS) $(NOCCS_CFLAGS) -c -o $@ $$src
+
+endif
